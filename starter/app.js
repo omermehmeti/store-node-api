494085b2f134dc9edf8ff06e7831acc32c96088d
Omer Mehmeti <58736018+omermehmeti@users.noreply.github.com>
2023-02-22 19:52:49 +0100

    Last Changes

---
+++ starter/app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+require('express-async-errors')
+
+// async errors
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// middleware 
+app.use(express.json());
+
+// routes
+const productsRoute= require('./routes/products')
+app.get('/',(req,res)=>{
+    res.send('<h1>Store Api</h1><a href=/api/v1/products>Products</a>')
+})
+
+/// Products route
+app.use('/api/v1/products',productsRoute);
+
+const connectDb = require('./db/connect');
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT|| 3000
+const start = async()=>{
+    try {
+        // connect db
+        await connectDb(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start();
